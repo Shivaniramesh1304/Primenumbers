41ce80aebf1661b522997582b8230f45f0deaf8a
Shivaniramesh1304 <shivaniramesh1304@gmail.com>
2025-01-30 21:54:19 +0530

    Add files via upload

---
+++ Primenumber.java
@@ -0,0 +1,22 @@
+public class Primenumber {
+        public static boolean isPrime(int number){
+            if(number <= 1){
+                return false;
+            }
+            for(int i=2;i< number;i++){
+                if(number % i ==0){
+                    return false;
+                }
+            }
+        
+        return true;
+    }
+    public static void main(String[]args){
+        int num=7;
+        if(isPrime(num)){
+            System.out.println(num + "is a prime number");
+        }else{
+            System.out.println(num+"is not a prime number");
+        }
+    }
+    }
